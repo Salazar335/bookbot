56e4818f853958d9d774840b216160f3c28ad444
Salazar <stenlybarreau@gmail.com>
2025-03-24 10:41:05 -0400

    bookbot program v1

---
+++ test.py
@@ -0,0 +1,14 @@
+item_prices = {
+        "health_potion": 10,
+        "mana_potion": 12,
+        "gold_dust": 5,
+        "dwarven_ale": 8,
+        "enchanted_scroll": 25,
+        "ice_cold_milk": 50,
+        "herbs": 7,
+        "crystal_shard": 20,
+        "magic_ring": 100,
+        "mystic_amulet": 150,
+    }
+dic_list = [item_prices]
+print(dic_list)
